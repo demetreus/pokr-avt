4ec331db6e4d2543e7ad28f586b387535dd04ef2
Demetreus <dmitrick0@gmail.com>
2019-02-27 09:15:23 +0200

    Formatting the contents of the url string

--- src/config/init.php
+++ src/config/init.php
@@ -15,3 +15,10 @@ define("LIBS", ROOT . '/vendor/Framework/Core/Libs');
 define("CACHE", ROOT . '/tmp/cache');
 define("CONFIG", ROOT . '/config');
 define("LAYOUT", 'default');
+
+// Formatting the contents of the url string
+$protocol = strtolower(substr($_SERVER["SERVER_PROTOCOL"], 0, strpos($_SERVER["SERVER_PROTOCOL"], '/'))) . '://';
+$app_path = "{$protocol}{$_SERVER['HTTP_HOST']}{$_SERVER['PHP_SELF']}";
+$app_path = preg_replace("#[^/]+$#", '', $app_path);
+$app_path = str_replace('/public/', '', $app_path);
+
