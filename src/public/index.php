06f7d7a875a852c6d15b54868a8e021fe3fdde1d
Demetreus <dmitrick0@gmail.com>
2019-02-27 07:56:06 +0200

    Improved server configuration file

---
+++ src/public/index.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Single entry point to the application
+ * All redirections are sent to the file via the .htaccess file
+ *
+ * @author Demetreus this.deetreus@gmail.com
+ *
+ */
+
+echo 'Hello World!';
