171254f500f21400dca47211ae099f497f4a5069
Dmytryi Kapeliukh <this.demetreus@gmail.com>
2019-02-27 21:11:05 +0200

    Created default template

---
+++ src/App/Views/layouts/default.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+    <?=$this->getMeta();?>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta charset="utf-8">
+</head>
+
+<body>
+    <h1>Шаблон по умолчаниыю</h1>
+    <?=$content.PHP_EOL;?>
+
+    <?php
+    // Debugging for database queries
+    $logs = \R::getDatabaseAdapter()->getDatabase()->getLogger();
+    debug($logs->grep('SELECT'));
+    ?>
+</body>
+</html>
