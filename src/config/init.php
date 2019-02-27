3f767951b80cb142a30b85cb3da6d78b68b81190
Demetreus <dmitrick0@gmail.com>
2019-02-27 09:07:01 +0200

    Main constants defined

---
+++ src/config/init.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Global application configuration
+ *
+ * @author Demetreus this.deetreus@gmail.com
+ *
+ */
+
+define("DEBUG", true);
+define("ROOT", dirname(__DIR__));
+define("WWW", ROOT . '/public');
+define("APP", ROOT . '/App');
+define("CORE", ROOT . '/vendor/Framework/Core');
+define("LIBS", ROOT . '/vendor/Framework/Core/Libs');
+define("CACHE", ROOT . '/tmp/cache');
+define("CONFIG", ROOT . '/config');
+define("LAYOUT", 'default');
