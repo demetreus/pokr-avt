b3afefc77a3444b1684b5588c3d7220bfa407763
Dmytryi Kapeliukh <this.demetreus@gmail.com>
2019-02-28 03:02:02 +0200

    Created and configured a connection to the database.

--- src/vendor/Framework/Core/Db.php
+++ src/vendor/Framework/Core/Db.php
@@ -2,7 +2,12 @@
 
 namespace Framework;
 
-
+/**
+ * Class Db
+ * @package Framework
+ *
+ * @author Demetreus this.demetreus@gmail.com
+ */
 class Db
 {
     use TSingleton;
@@ -11,6 +16,16 @@ class Db
     {
         $db = require_once CONFIG . '/settings.db.php';
 
-        
+        class_alias('\RedBeanPHP\R', '\R');
+        \R::setup($db['dsn'], $db['user'], $db['pass']);
+
+        if (!\R::testConnection()) {
+            throw new \Exception("Could not connect to database", 500);
+        }
+
+        \R::freeze(true);
+
+        if (DEBUG) \R::debug(true, 1);
+
     }
 }
